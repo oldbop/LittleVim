3b62a815d022f1a76e8a17c73849e6d5a8149606
oldbop <oliverdavybowker@gmail.com>
2024-01-28 21:40:06 +0000

    Add vsnip navigation keymaps

--- init.lua
+++ init.lua
@@ -69,6 +69,22 @@ vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 
+vim.keymap.set({ "i", "s" }, "<Tab>", function()
+  if vim.fn["vsnip#jumpable"](1) == 1 then
+    return "<Plug>(vsnip-jump-next)"
+  else
+    return "<Tab>"
+  end
+end, { expr = true, remap = true })
+
+vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
+  if vim.fn["vsnip#jumpable"](-1) == 1 then
+    return "<Plug>(vsnip-jump-prev)"
+  else
+    return "<S-Tab>"
+  end
+end, { expr = true, remap = true })
+
 -- Completion configuration
 local cmp = require("cmp")
 
